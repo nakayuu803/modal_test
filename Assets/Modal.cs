94d12a585e544463b5d6b948bdd5a313389eb6c0
nakayuu803 <ynakamura704@gmail.com>
2023-08-23 17:12:04 +0900

    pos_main

--- Assets/Modal.cs
+++ Assets/Modal.cs
@@ -8,26 +8,33 @@ public class Modal : MonoBehaviour
 {
     public Transform parent;
     public InputField inputField;
-
-    public void create(){
+    
+    public void create_error(){
         GameObject modal = (GameObject)Resources.Load("filter_error");
         Instantiate(modal, parent);
     }
-    public void destroy()
-    {
-        GameObject modal = GameObject.Find("filter_error(Clone)");
-        if(modal == null){
-            modal = GameObject.Find("filter_name(Clone)");
-            GameObject name = GameObject.Find("name");
-            inputField = inputField.GetComponent<InputField> ();
-            Text work_name = name.GetComponent<Text> ();
-            work_name.text = inputField.text;
-        }
-        Destroy(modal);
-    }
+   
     public void create_name(){
         GameObject modal = (GameObject)Resources.Load("filter_name");
         Instantiate(modal, parent);
 
     }
+
+    public void destroy_error()
+    {
+        GameObject modal = GameObject.Find("filter_error(Clone)");
+        Destroy(modal);
+
+    }
+    
+    public void destroy_name()
+    {
+        GameObject modal = GameObject.Find("filter_name(Clone)");
+        GameObject name = GameObject.Find("name");
+        inputField = inputField.GetComponent<InputField> ();
+        Text work_name = name.GetComponent<Text> ();
+        work_name.text = inputField.text;
+        Destroy(modal);
+    }
+        
 }
